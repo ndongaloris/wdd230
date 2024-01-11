d472a6bedb17e19e398c253cfe794a32ffdfce05
ndongaloris <ndongajared@gmail.com>
2024-01-11 20:07:47 +0200

    css overview fixed

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,14 @@ hamButton.addEventListener('click', () =>{
 
 const modeButton = document.querySelector("#modeButton");
 const body = document.querySelector('body');
-// const nav = document.querySelector('nav');
+const nav = document.querySelector('nav');
 
 modeButton.addEventListener('click', () =>{
         body.classList.toggle('dark');
         if (document.body.classList.contains('dark')){
             modeButton.src = 'images/light-mode.svg';
             hamButton.style.background = 'white';
+            nav.style.color = 'white';
         }
         else{
             modeButton.src = 'images/dark-mode.svg';
