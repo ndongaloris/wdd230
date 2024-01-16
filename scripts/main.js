0447d0d48537a9edd519e970365fb4021354a151
ndongaloris <ndongajared@gmail.com>
2024-01-16 18:52:48 +0200

    temple added and google map

--- scripts/main.js
+++ scripts/main.js
@@ -9,15 +9,26 @@ hamButton.addEventListener('click', () =>{
 const modeButton = document.querySelector("#modeButton");
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
-
 modeButton.addEventListener('click', () =>{
-        body.classList.toggle('dark');
-        if (document.body.classList.contains('dark')){
-            modeButton.src = 'images/light-mode.svg';
-            hamButton.style.background = 'white';
-            nav.style.color = 'white';
-        }
+    body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')){
+        modeButton.src = 'images/light-mode.svg';
+        hamButton.style.background = 'white';
+        nav.style.color = 'white';
+    }
         else{
             modeButton.src = 'images/dark-mode.svg';
         }
 })
+
+
+let numVisits = parseInt(window.localStorage.getItem('numVisits')) || 0;
+const visit = document.querySelector("#visit");
+if (parseInt(visit === 0)){
+    visit.textContent = `This is your very first time visiting`;
+}
+else{
+    visit.textContent = numVisits;
+}
+numVisits++;
+localStorage.setItem("numVisits", numVisits);
