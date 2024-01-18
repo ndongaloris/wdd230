bed02b92da053bceda90693d88ccbae26d9f7363
ndongaloris <ndongajared@gmail.com>
2024-01-18 23:44:38 +0200

    fixing error

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -6,20 +6,20 @@ hamButton.addEventListener('click', () =>{
     nav.classList.toggle('open');
 })
 
-// const visit = document.querySelector('#visit');
-// let numVisits = Number(window.localStorage.getItem('count'))|| 0;
-// let todayDate = Number(window.localStorage.getItem('date')) || 0;
-// let theDate = new Date().getDay();
-// if (numVisits === 0){
-//     visit.textContent = "Welcome! Let us know if you have any questions.";
-// }
-// // else if((todayDate - theDate) < 1) {
-// //     visit.textContent = "Back so soon! Awesome!";
-// // }
-// else{
-//     visit.textContent = `You last visited ${todayDate - theDate} days ago.` ;
-// }
+const visit = document.querySelector('#visit');
+let numVisits = Number(window.localStorage.getItem('count'))|| 0;
+let todayDate = Number(window.localStorage.getItem('date')) || 0;
+let theDate = new Date().getDay();
+if (numVisits === 0){
+    visit.textContent = "Welcome! Let us know if you have any questions.";
+}
+else if((todayDate - theDate) < 1) {
+    visit.textContent = "Back so soon! Awesome!";
+}
+else{
+    visit.textContent = `You last visited ${todayDate - theDate} days ago.` ;
+}
 
-// numVisits++;
-// localStorage.setItem('count', numVisits);
-// localStorage.setItem('date', theDate);
+numVisits++;
+localStorage.setItem('count', numVisits);
+localStorage.setItem('date', theDate);
