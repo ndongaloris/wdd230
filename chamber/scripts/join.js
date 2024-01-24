462b39f133190b5ff543bf09acbe26cf26a3dd16
ndongaloris <ndongajared@gmail.com>
2024-01-24 13:34:23 +0200

    link and toggle

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,11 @@
+const hamButton = document.querySelector('#menu');
+const nav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () =>{
+    hamButton.classList.toggle('open');
+    nav.classList.toggle('open');
+})
+
 const dateTime = document.querySelector('#timestamp');
 
 const dates = new Date();
