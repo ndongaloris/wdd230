e9f64d78759de9afa3948b22baef125771271ab1
ndongaloris <ndongajared@gmail.com>
2024-01-30 17:21:48 +0200

    json repair

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,9 +6,54 @@ hamButton.addEventListener('click', () =>{
     nav.classList.toggle('open');
 })
 
-const section = document.createElement('section');
+const url = "https://ndongaloris.github.io/wdd230/chamber/data/members.json";
+
+async function getAPI(url){
+    const response = await fetch(url);
+    try{
+        if (response.ok){
+            const data = await response.json();
+            populate(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+const main = document.querySelector('main');
+
 const mainHeader = document.createElement('h1');
-const name = document.createElement('p');
-const address = document.createElement('p');
-const number = document.createElement('p');
-const links = document.createElement('a');
+mainHeader.textContent = "Beto Na Beto Directory";
+main.appendChild(mainHeader);
+
+function populate(data){
+    data['companies'].forEach(company => {
+        
+        const section = document.createElement('div');
+        const name = document.createElement('p');
+        const links = document.createElement('a');
+        const address = document.createElement('p');
+        const number = document.createElement('p');
+
+        name.textContent = company.names;
+        address.textContent = company.addresses;
+        links.href = company.website;
+        links.textContent = company.website;
+        number.text = company.phone;
+
+        section.appendChild(name);
+        section.appendChild(address);
+        section.appendChild(links);
+        section.appendChild(number);
+
+        main.appendChild(section);
+    });
+
+
+}
+
+
+getAPI(url);
